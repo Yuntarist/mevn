b668ee929998b47e07d3576b0510c45c6db6f29b
Yuntarist <ftaruftk@naver.com>
2023-02-17 14:33:42 +0900

    230217

--- mongoDB230116/p347_CRUD_Create.js
+++ mongoDB230116/p347_CRUD_Create.js
@@ -1,7 +1,8 @@
+const dtd = 123456
 const { Photo } = require('./p346_CRUD_Ori.js')
 const main = async () => {
   const _data = {
-    albumId: 230112,
+    albumId: dtd,
     id: 230112,
     title: 'ㅇㅅㅇ',
     url: 'google.com',
